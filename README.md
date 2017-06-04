f7c01ea62eb18b3eb2a4ad232e63b98b0daa5c91
xiaoyuanzheng <lyzheng1025@163.com>
2017-06-05 00:10:30 +0800

    emeny 和 palyer的运动

--- README.md
+++ README.md
@@ -5,3 +5,21 @@
 学生应该用这个[评审标准](https://review.udacity.com/#!/rubrics/499/view))来自我检查自己提交的代码。 确认自己写的函数要是**面向对象的** -  要么是类函数（就像函数 Player 和 Enemy）要么是类的原型链上的函数比如 Enemy.prototype.checkCollisions ， 在类函数里面或者类的原型链函数里面适当的使用关键词 'this' 来引用调用该函数的对象实例。最后保证你的**readme.md**文件要写明关于如何运行和如何玩你的街机游戏的指引。
 
 关于如何开始这个项目的更详细的指导，可以查阅这份[指南](https://gdgdocs.org/document/d/1v01aScPjSWCCWQLIpFqvg3-vXLH2e8_SZQKC8jNO0Dc/pub?embedded=true)。
+
+
+
+1. ## 需完成的工作
+	1. 在提供的范本 - Enemy 类(Class)的基础上创建 Player 类
+    2. 填 Player 实例(instance)起始的位置信息(提示: x? y?)
+    3. 用 Player 类创造一个player实例 (你就会看到游戏场景被启动了)
+    4. 使用 Enemy 类创造多个 enemy 实例
+    5. 填入 enemy 实例的起始位置信息
+    6. 填入 enemy 实例的移动函数(function)
+    7. 建立 player 的输入(input)函数 (Brian注: 以控制player的上下左右移动)
+    8. 建立检测碰撞的函数
+		
+	註: 你不需要更改任何在 engine.js 或 resources.js 里的代码，你 只需 在 app.js 里写代码。Sprite(游戏里的角色)的位置不太好调，但只要多调整几次就行了。
+
+	若能读懂此项目所提供的JS档里的注释最好，但那并不容易。
+	
+	(Brian注: 但我个人建议 一定要彻底把app.js 里的注释看懂)
--- js/app.js
+++ js/app.js
@@ -1,17 +1,34 @@
+var enemy_debug = 0;
+var player_debug = 1;
+function GetRandomNum(Min,Max)
+{   
+    var Range = Max - Min;   
+    var Rand = Math.random();   
+    return(Min + Math.round(Rand * Range));   
+}   
+
 // 这是我们的玩家要躲避的敌人 
-var Enemy = function() {
+var Enemy = function(x,y) {
     // 要应用到每个敌人的实例的变量写在这里
     // 我们已经提供了一个来帮助你实现更多
-
+    this.x = x;
+    this.y = y;
+    this.speed = GetRandomNum(10,101);
+    if(enemy_debug)console.log("enemy speed:"+this.speed);
     // 敌人的图片或者雪碧图，用一个我们提供的工具函数来轻松的加载文件
     this.sprite = 'images/enemy-bug.png';
 };
 
 // 此为游戏必须的函数，用来更新敌人的位置
 // 参数: dt ，表示时间间隙
+Enemy.prototype.width = 101;
 Enemy.prototype.update = function(dt) {
     // 你应该给每一次的移动都乘以 dt 参数，以此来保证游戏在所有的电脑上
     // 都是以同样的速度运行的
+    if(enemy_debug)console.log('location of enemy,x:'+this.x+'y:'+this.y);
+    this.x = this.x + this.speed*dt;
+    this.x = this.x > (ctx.width + this.width) ? 0 : this.x;
+    this.render();
 };
 
 // 此为游戏必须的函数，用来在屏幕上画出敌人，
@@ -21,12 +38,75 @@ Enemy.prototype.render = function() {
 
 // 现在实现你自己的玩家类
 // 这个类需要一个 update() 函数， render() 函数和一个 handleInput()函数
+var Player = function()
+{
+    this.x = 101 * 2;
+    this.y = 83 * 5; 
+    this.sprite = 'images/char-boy.png';
+}
+
+Player.prototype.update = function()
+{
+    if(player_debug)console.log('location of player,x:'+this.x+",y:"+this.y);
+    this.render();
+}
+
+Player.prototype.render = function()
+{
+    ctx.drawImage(Resources.get(this.sprite),this.x,this.y);
+}
+
+Player.prototype.handleInput = function(direction)
+{
+    switch(direction)
+    {
+        case 'left':
+            this.x -= 101;
+            if(player_debug)console.log('x - 101:'+this.x);
+            break;
+        case 'right':
+            this.x += 101;
+            if(player_debug)console.log('x + 101:'+this.x);
+            break;
+        case 'up':
+            this.y -= 83;
+            if(player_debug)console.log('y - 83:'+this.y);
+            break;
+        case 'down':
+            this.y += 83;
+            if(player_debug)console.log('y + 83:'+this.y);
+            break;
+    }
+
+    if(this.x < 0) this.x = 0;
+    if(this.x > 4 * 101)this.x = 4 * 101;
+    if(this.y < 0) this.y = 0;
+    if(this.y > 5 * 83)this.y = 5 * 83;
+    this.update();
+}
 
 
 // 现在实例化你的所有对象
 // 把所有敌人的对象都放进一个叫 allEnemies 的数组里面
 // 把玩家对象放进一个叫 player 的变量里面
+window.allEnemies = [];
+for(var index = 0;index < 3; index ++)
+{
+    (
+        function(row)
+        {
+            setInterval(function()
+            {
+                var locY = (row + 0.5) * 83;
+                if(enemy_debug)console.log('add a enemy,x:0, y:' + locY);
+                allEnemies.push(new Enemy(-101,locY));
+            },5000);
+        }
+    )(index);
+}
 
+
+window.player = new Player();
 
 // 这段代码监听游戏玩家的键盘点击事件并且代表将按键的关键数字送到 Play.handleInput()
 // 方法里面。你不需要再更改这段代码了。
@@ -37,6 +117,7 @@ document.addEventListener('keyup', function(e) {
         39: 'right',
         40: 'down'
     };
+    if(player_debug)console.log('keyup occure,keyCode:'+e.keyCode);
 
     player.handleInput(allowedKeys[e.keyCode]);
 });
